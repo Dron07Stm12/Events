14c05dfa35bd9195c8dbf25bcbd464cc7ac9771e
Dron07Stm12 <dronperenen@gmail.com>
2023-04-27 13:30:59 +0300

    использование анонимного метода, лямбды с аргументом

--- Program.cs
+++ Program.cs
@@ -7,143 +7,55 @@ using System.Threading.Tasks;
 
 
 // Объявить тип делегата для события, 
-public  delegate void MyEventHandler();
+public  delegate void MyEventHandler(int i);
 
 namespace Events
 {
     public class MyEvent 
     {
+        public event MyEventHandler SomeEvent;
 
-        MyEventHandler[] handler = new MyEventHandler[3];
-
-        public event MyEventHandler SomeEvent 
+        //Метод для запуска события
+        public void Method(int n) 
         {
-            
-            add 
+            if (SomeEvent != null)
             {
-                int i;
-                for (i = 0; i < 3; i++)
-                {
-                    if (handler[i] == null)
-                    {
-                        handler[i] = value;
-                        break;
-                    }
-                }
-
-                if (i == 3)
-                {
-                    Console.WriteLine("Список событий заполнен");
-                }
-            
-            }
-            //Когда же обработчик событий удаляется 
-            //из цепочки событий, то вызывается аксессор remove и в массиве evnt осуществляется
-             //поиск ссылки на этот обработчик, передаваемой в качестве параметра value. 
-            remove 
-            {
-                int i;
-                for ( i = 0; i < 3; i++)
-                {
-                    if (handler[i] == value)
-                    {
-                        handler[i] = null;
-                    }
-                }
-
-                if (i == 3)
-                {
-                    Console.WriteLine("Обработчик событий не найден");
-                }
-            }
-             
-        }
-
-        public void Method_Event() 
-        {
-            for (int i = 0; i < handler.Length; i++)
-            {
-                if (handler[i] != null)
-                {
-                    handler[i]();
-                }
+                SomeEvent(n);
             }
         
-        
-        }
-
-        public void Method_Event2()
-        {
-            
-                    handler[0]();
-                
-            
-
-
         }
 
 
     }
 
-    public class X 
-    {   
-        public  void Hendler_x() 
-        {
-            Console.WriteLine("Событие обьекта класса Х ");
-        }   
-    }
-
-
-    public class Y
-    {
-        public void Hendler_y()
-        {
-            Console.WriteLine("Событие обьекта класса Y ");
-        }
-    }
-
-
-    public class Z
-    {
-        public void Hendler_z()
-        {
-            Console.WriteLine("Событие обьекта класса Z ");
-        }
-    }
-
-
+    
 
     public class Program
     {
+
+        static void Method_Program(int i)
+        { 
+            Console.WriteLine("Добавляем в список событий статический метод " + i);
+        }
      
         static void Main(string[] args)
         {
             MyEvent my = new MyEvent();
-            X x = new X();
-            Y y = new Y();
-            Z z = new Z();
 
+           // добавим обработку в список события
+            my.SomeEvent += delegate (int i)
+            {
+                Console.WriteLine("Добавляем в список событий анонимный метод номер " + i);
+            };
 
-            //Когда в цепочку событий добавляется обработчик событий, вызывается аксессор 
-            //add, и в первом неиспользуемом(т.е.пустом) элементе массива evnt запоминается
-            //ссылка на этот обработчик, содержащаяся в неявно задаваемом параметре value.
-            my.SomeEvent += x.Hendler_x;
-            my.SomeEvent += y.Hendler_y;
-            my.SomeEvent += z.Hendler_z;
-            //будет писать список событий заполнен
-         //   my.SomeEvent += x.Hendler_x;
-            my.Method_Event();
+            my.SomeEvent += n => Console.WriteLine("lymbda "+ n);
 
-            // Удалить обработчик. будет писать дважды обработчик события не найден
-            //   my.SomeEvent -= x.Hendler_x;
-            Console.WriteLine();
-            my.SomeEvent -= z.Hendler_z;
-            my.Method_Event();
-            //Обратно добавим обработчик событий
-            Console.WriteLine();
-            my.SomeEvent += z.Hendler_z;
-            my.Method_Event();
-            my.Method_Event2();
+          
+            my.SomeEvent += Program.Method_Program;
+            //запустим событие
+            my.Method(1);
+          //  my.Method(2);
+           
 
         }
     }
