aeb49ef0cd1c54cdf6224f9b960ffa2440c0805c
Dron07Stm12 <dronperenen@gmail.com>
2023-04-27 17:38:45 +0300

    делегат  для среды.NET Framewor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -6,56 +7,85 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-// Объявить тип делегата для события, 
-public  delegate void MyEventHandler(int i);
 
 namespace Events
 {
-    public class MyEvent 
+    //1 - создается класс MyEventArgs, производный от класса EventArgs. 
+    public class MyEventArgs : EventArgs
     {
-        public event MyEventHandler SomeEvent;
-
-        //Метод для запуска события
-        public void Method(int n) 
-        {
-            if (SomeEvent != null)
-            {
-                SomeEvent(n);
-            }
-        
-        }
+        public int EventNum;
 
 
     }
+    //объявляется делегат MyEventHandler, принимающий два параметра, требующиеся 
+    //для среды.NET Framework, и  под событие
+    // первый параметр содержит  
+    // ссылку на объект, формирующий событие, а второй параметр — ссылку на объект класса
+    //EventArgs или производного от него класса.
+    public delegate void MyEventHandler(object source, MyEventArgs e);
 
+
+    public class MyEvent 
+    {
+        public  static int count;
+        //событие
+        public event MyEventHandler SomeEvent;
+       
+        //создадим метод для запуска события
+        public void Method_SomeEvent() 
+        {
+            MyEventArgs args = new MyEventArgs();
+            if (SomeEvent != null)
+            {
+                args.EventNum = count++;
+                SomeEvent(this,args);
+            }
+        
+        }
     
+    }
+
+    //Обработчики событий Handler (),  
+    //определяемые в классах X и Y, принимают параметры тех же самых типов.
+
+    public class X 
+    {
+        public void Handler(object source,MyEventArgs args)
+        {
+            Console.WriteLine("Событие " + args.EventNum + " получено обьектом класса Х ");
+            Console.WriteLine("Источник(ссылка на обьект формирующие событие): " + source);
+        
+        } 
+    
+    }
+
+    public class Y 
+    {
+
+        public void Handler(object source, MyEventArgs eventArgs) 
+        {
+            Console.WriteLine("Cобытие " + eventArgs.EventNum + " получено обьектом класса Y ");
+            Console.WriteLine("Источник(ссылка на обьект формирующие событие): " + source);
+        }
+    
+    }
+
+
 
     public class Program
     {
 
-        static void Method_Program(int i)
-        { 
-            Console.WriteLine("Добавляем в список событий статический метод " + i);
-        }
-     
+       
         static void Main(string[] args)
         {
-            MyEvent my = new MyEvent();
-
-           // добавим обработку в список события
-            my.SomeEvent += delegate (int i)
-            {
-                Console.WriteLine("Добавляем в список событий анонимный метод номер " + i);
-            };
-
-            my.SomeEvent += n => Console.WriteLine("lymbda "+ n);
-
-          
-            my.SomeEvent += Program.Method_Program;
-            //запустим событие
-            my.Method(1);
-          //  my.Method(2);
+            MyEvent myEvent = new MyEvent();
+            X x = new X();
+            Y y = new Y();
+            myEvent.SomeEvent += x.Handler;
+            myEvent.SomeEvent += y.Handler;
            
+            myEvent.Method_SomeEvent();
+            myEvent.Method_SomeEvent();
 
         }
     }
